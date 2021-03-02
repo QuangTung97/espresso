0b596a80fc3737583c115065a1eda15a03e78832
Tùng Tạ Quang <tung.tq@teko.vn>
2021-03-02 14:48:39 +0700

    New Allocator

--- allocator/allocator.go
+++ allocator/allocator.go
@@ -10,28 +10,53 @@ type SlabConfig struct {
 
 // Config ...
 type Config struct {
-	Slabs []SlabConfig
+	MemLimit int
+	Slabs    []SlabConfig
 }
 
 // Allocator ...
 type Allocator struct {
 	buddy Buddy
 	slabs []*Slab
+
+	memoryUsage int
+}
+
+func findMinSizeLog(slabs []SlabConfig) uint32 {
+	min := slabs[0].ChunkSizeLog
+	for _, s := range slabs[1:] {
+		if min > s.ChunkSizeLog {
+			min = s.ChunkSizeLog
+		}
+	}
+	return min
+}
+
+func findSizeMultiple(minSizeLog uint32, limit int) uint32 {
+	mask := 1<<minSizeLog - 1
+	return uint32((limit + mask) >> minSizeLog)
+}
+
+func allocateData(minSizeLog uint32, sizeMultiple uint32) []uint64 {
+	return make([]uint64, sizeMultiple<<(minSizeLog-6))
 }
 
 // New ...
 func New(conf Config) *Allocator {
-	// TODO continue New
-	data := make([]uint64, 1<<17)
+	minSizeLog := findMinSizeLog(conf.Slabs)
+	sizeMultiple := findSizeMultiple(minSizeLog, conf.MemLimit)
+
+	data := allocateData(minSizeLog, sizeMultiple)
 
 	result := &Allocator{}
-	BuddyInit(&result.buddy, 12, 20, unsafe.Pointer(&data[0]))
+	BuddyInit(&result.buddy, minSizeLog, sizeMultiple, unsafe.Pointer(&data[0]))
 
 	slabs := make([]*Slab, 0, len(conf.Slabs))
 	for _, slabConf := range conf.Slabs {
 		slabs = append(slabs, NewSlab(&result.buddy, slabConf.ElemSize, slabConf.ChunkSizeLog))
 	}
 	result.slabs = slabs
+	result.memoryUsage = 0
 
 	return result
 }
--- allocator/allocator_test.go
+++ allocator/allocator_test.go
@@ -5,8 +5,113 @@ import (
 	"testing"
 )
 
+func TestFinMinSizeLog(t *testing.T) {
+	slabs := []SlabConfig{
+		{
+			ElemSize:     88,
+			ChunkSizeLog: 12,
+		},
+	}
+	assert.Equal(t, uint32(12), findMinSizeLog(slabs))
+
+	slabs = []SlabConfig{
+		{
+			ElemSize:     88,
+			ChunkSizeLog: 14,
+		},
+		{
+			ElemSize:     88,
+			ChunkSizeLog: 11,
+		},
+	}
+	assert.Equal(t, uint32(11), findMinSizeLog(slabs))
+}
+
+func TestFindSizeMultiple(t *testing.T) {
+	table := []struct {
+		name     string
+		minSize  uint32
+		limit    int
+		expected uint32
+	}{
+		{
+			name:     "normal",
+			minSize:  12,
+			limit:    1234 << 10,
+			expected: 309,
+		},
+		{
+			name:     "near-next-size-multiple",
+			minSize:  12,
+			limit:    1235 << 10,
+			expected: 309,
+		},
+		{
+			name:     "next-size-multiple",
+			minSize:  12,
+			limit:    1236 << 10,
+			expected: 309,
+		},
+		{
+			name:     "next-size-multiple",
+			minSize:  12,
+			limit:    1237 << 10,
+			expected: 310,
+		},
+		{
+			name:     "more-than-4GB",
+			minSize:  12,
+			limit:    5 << 30,
+			expected: 5 << 18,
+		},
+	}
+
+	for _, e := range table {
+		t.Run(e.name, func(t *testing.T) {
+			result := findSizeMultiple(e.minSize, e.limit)
+			assert.Equal(t, e.expected, result)
+		})
+	}
+}
+
+func TestAllocateData(t *testing.T) {
+	table := []struct {
+		name         string
+		minSizeLog   uint32
+		sizeMultiple uint32
+		expected     int
+	}{
+		{
+			name:         "normal",
+			minSizeLog:   6,
+			sizeMultiple: 20,
+			expected:     20,
+		},
+		{
+			name:         "normal",
+			minSizeLog:   8,
+			sizeMultiple: 20,
+			expected:     20 << 2,
+		},
+		{
+			name:         "5-GB",
+			minSizeLog:   12,
+			sizeMultiple: 5 << 18,
+			expected:     5 << 24,
+		},
+	}
+
+	for _, e := range table {
+		t.Run(e.name, func(t *testing.T) {
+			data := allocateData(e.minSizeLog, e.sizeMultiple)
+			assert.Equal(t, e.expected, len(data))
+		})
+	}
+}
+
 func TestAllocatorNew(t *testing.T) {
 	conf := Config{
+		MemLimit: 1234 << 10,
 		Slabs: []SlabConfig{
 			{
 				ElemSize:     88,
@@ -18,11 +123,25 @@ func TestAllocatorNew(t *testing.T) {
 			},
 			{
 				ElemSize:     128,
-				ChunkSizeLog: 12,
+				ChunkSizeLog: 13,
 			},
 		},
 	}
 
 	alloc := New(conf)
+	assert.Equal(t, uint32(309), alloc.buddy.sizeMultiple)
+	assert.Equal(t, uint32(12), alloc.buddy.minSize)
+
 	assert.Equal(t, 3, len(alloc.slabs))
+
+	assert.Equal(t, uint32(88), alloc.slabs[0].elemSize)
+	assert.Equal(t, uint32(12), alloc.slabs[0].chunkSizeLog)
+
+	assert.Equal(t, uint32(102), alloc.slabs[1].elemSize)
+	assert.Equal(t, uint32(12), alloc.slabs[1].chunkSizeLog)
+
+	assert.Equal(t, uint32(128), alloc.slabs[2].elemSize)
+	assert.Equal(t, uint32(13), alloc.slabs[2].chunkSizeLog)
+
+	assert.Equal(t, 0, alloc.memoryUsage)
 }
